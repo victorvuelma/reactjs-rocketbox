6faaf667bb50972fa9767cb05ede49fd689c1e3c
Victor Vuelma <victorvuelma@outlook.com>
2019-08-27 18:46:19 -0300

    add main page state

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
+import api from '../../services/api';
 
 import logo from '../../assets/logo.svg';
 import './styles.css';
 
 export default class Main extends Component {
+
+  state = {
+    newBox: '',
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log(this.state.newBox)
+  }
+
+  handleInputChange = (e) => {
+    this.setState({ newBox: e.target.value})
+  }
+
   render() {
     return (
       <div id="main-container">
-        <form action="">
+        <form onSubmit={this.handleSubmit}>
           <img src={logo} alt="RocketBox Logo" />
-          <input type="text" placeholder="Criar um box"/>
+          <input type="text" 
+              placeholder="Criar um box" 
+              value={this.state.newBox}
+              onChange={this.handleInputChange} />
           <button type="submit">Criar</button>
         </form>
       </div>
