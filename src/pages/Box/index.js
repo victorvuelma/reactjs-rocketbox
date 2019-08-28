18412ca32449429c192ef0d8b9c5eb62cb23720c
Victor Vuelma <victorvuelma@outlook.com>
2019-08-28 17:58:28 -0300

    show box files

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -2,35 +2,44 @@ import React, { Component } from 'react';
 
 import { MdInsertDriveFile } from 'react-icons/md';
 
+import api from '../../services/api';
+
 import logo from '../../assets/logo.svg';
 import './style.css';
 
 export default class Box extends Component {
+
+  state = {
+    box: {}
+  }
+
+  async componentDidMount() {
+    const box = this.props.match.params.id;
+
+    const response = await api.get(`boxes/${box}`);
+
+    this.setState({box: response.data})
+  }
+
   render() {
     return (
       <div id="box-container">
         <header>
           <img src={logo} alt="" />
-          <h1>Rocketseat</h1>
+          <h1>{this.state.box.title}</h1>
         </header>
 
         <ul>
-          <li>
-            <a className="fileInfo" href="">
-              <MdInsertDriveFile size={24} color="#A5CFFF" />
-              <strong>Desafio.pdf</strong>
-            </a>
+          { this.state.box.files && this.state.box.files.map(file => (
+            <li>
+              <a className="fileInfo" href={file.url} target="_blank">
+                <MdInsertDriveFile size={24} color="#A5CFFF" />
+                <strong>{file.title}</strong>
+              </a>
 
-            <span>há 3 minutos atrás</span>
-          </li>
-          <li>
-            <a className="fileInfo" href="">
-              <MdInsertDriveFile size={24} color="#A5CFFF" />
-              <strong>Desafio.pdf</strong>
-            </a>
-
-            <span>há 3 minutos atrás</span>
-          </li>
+              <span>{file.createdAt}</span>
+            </li>
+          ))}
         </ul>
       </div>
     );
