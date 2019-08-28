3cbb38ac8b2ae38efa1cd69cd6a206a7d2f26d87
Victor Vuelma <victorvuelma@outlook.com>
2019-08-28 17:40:20 -0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
 
-import Main from './pages/Main';
+import Routes from './routes';
 
 function App() {
   return (
-    <Main />
+    <Routes />
   );
 }
 
