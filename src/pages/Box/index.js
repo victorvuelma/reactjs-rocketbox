d210d638366bdaa8825b930d62806f8a11f085a7
Victor Vuelma <victorvuelma@outlook.com>
2019-08-28 17:51:25 -0300

    add box page structure

--- src/pages/Box/index.js
+++ src/pages/Box/index.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
 
+import { MdInsertDriveFile } from 'react-icons/md';
+
+import logo from '../../assets/logo.svg';
 import './style.css';
 
 export default class Box extends Component {
   render() {
-    return <div />;
+    return (
+      <div id="box-container">
+        <header>
+          <img src={logo} alt="" />
+          <h1>Rocketseat</h1>
+        </header>
+
+        <ul>
+          <li>
+            <a className="fileInfo" href="">
+              <MdInsertDriveFile size={24} color="#A5CFFF" />
+              <strong>Desafio.pdf</strong>
+            </a>
+
+            <span>há 3 minutos atrás</span>
+          </li>
+          <li>
+            <a className="fileInfo" href="">
+              <MdInsertDriveFile size={24} color="#A5CFFF" />
+              <strong>Desafio.pdf</strong>
+            </a>
+
+            <span>há 3 minutos atrás</span>
+          </li>
+        </ul>
+      </div>
+    );
   }
 }
